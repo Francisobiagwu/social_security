afe70b7b1307984765a7c0bb4180697984e9baf5
Francis <francisobiagwu@gmail.com>
2018-03-01 18:07:55 -0500

    update

--- account.py
+++ account.py
@@ -2,7 +2,6 @@
 This is a program used to create user account and credentials for a user
 """
 
-
 accounts = {}
 
 
@@ -11,14 +10,13 @@ class Person:
     This is a class that creates and instance of a person
     """
 
-    def __init__(self, first_name, last_name, date_of_birth):
+    def __init__(self, first_name, last_name):
         """
 
         :rtype: object
         """
         self.first_name = first_name
         self.last_name = last_name
-        self.date_of_birth = date_of_birth
 
 
 def create_new_account():
@@ -31,9 +29,6 @@ def create_new_account():
 
         return first_name
 
-
-
-
     def get_last_name():
         last_name = str(input("Lastname: "))
         if len(last_name.strip()) < 2:
@@ -42,40 +37,34 @@ def create_new_account():
 
         return last_name
 
-    def get_date_of_birth():
-        birthday = str(input("Date of birth (MM/DD/YY): "))
-        if (len(birthday)) != 8:
-            print("Please enter a valid value for date")
-            get_date_of_birth()
+    def get_username():
+        user_name = str(input("Username: "))
+        return user_name
 
-        month = birthday[0:2]
-        day = birthday[3:5]
-        year = birthday[6:8]
+    def verify_username(username):
+        if username == firstname or username == lastname:
+            get_username()
 
-        date_format = False
-
-        if len(month) == 2:
-            if len(day) == 2:
-                if len(year) == 2:
-                    date_format = True
-
-        if not date_format:
-            get_date_of_birth()
-
-        return birthday
-
-    first_name = get_first_name()
-    last_name = get_last_name()
-    date_of_birth = get_date_of_birth()
-
-    username = str(input("Username: "))
+    firstname = get_first_name()
+    lastname = get_last_name()
+    username = get_username()
     flag = "Unlocked"
 
     # check if username already exist, if it does, request that the user enters an new username
 
-    accounts[username] = (last_name, first_name, date_of_birth, flag)
+    accounts[username] = (lastname, firstname, flag)
 
-    return first_name, last_name, date_of_birth, date_of_birth, flag
+    return firstname, lastname, flag
+
+def login():
+    user_name = input("username: ")
+    password = input("password: ")
+
+
+
+
+
 
 
 create_new_account()
+print(accounts.items())
