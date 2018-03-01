55c3f41f68639378a7e4f781dfc07b0ac6748ba3
Francis <francisobiagwu@gmail.com>
2018-03-01 16:33:20 -0500

    testing

---
+++ account.py
@@ -0,0 +1,22 @@
+
+class Person:
+    """
+    This is a class that creates and instance of a person
+    """
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_of_birth = ""
+
+
+
+
+def create_new_account():
+    first_name = str(input("Firstname: "))
+    last_name = str(input("Lastname: "))
+    date_of_birth = str("Date of birth (MM/DD/YY: ")
+
+
+
+
