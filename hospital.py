5d8e8da8e59116db8386d4f55d83bf35e684fcb3
Francis <francisobiagwu@gmail.com>
2018-03-21 21:09:31 -0400

    update

--- hospital.py
+++ hospital.py
@@ -6,9 +6,11 @@ __date__ = 3 / 21 / 2018
 from Person import Person
 
 new_baby = Person('Francis', 'Obiagwu')
+another_baby = Person('Bob', 'Smith')
 
 # prints the firstname, lastname and the last four of ssn
 print(new_baby.first_name, new_baby.last_name, str(new_baby.social_security_number)[5:9])
+print(another_baby.first_name, another_baby.last_name, str(another_baby.social_security_number)[5:9])
 
 
 
