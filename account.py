c88c9b49f3017581ebd468f58965e9d512c3b92f
Francis <francisobiagwu@gmail.com>
2018-03-21 20:15:15 -0400

    update

--- account.py
+++ account.py
@@ -1,14 +1,13 @@
 """
 This is a program used to create user account and credentials for a user
 """
-import os
 
-path = "account_file"
+import hashlib
+import random
+
+persons_created = {}
 accounts = {}
 account_file_do_exist = False
-firstname = ""
-lastname = ""
-username = ""
 
 
 class Person:
@@ -23,133 +22,64 @@ class Person:
         """
         self.first_name = first_name
         self.last_name = last_name
+        self.social_security_number = 4
+
 
 
 def create_new_account():
-    def get_first_name():
-        first_name = str(input("Firstname: ")).strip()
-        if len(first_name.strip()) < 2:
-            print("Please enter a value for firstname. No space or initials")
-            get_first_name()
-
-        return first_name
-
-    def get_last_name():
-        last_name = str(input("Lastname: ")).strip()
-        if len(last_name.strip()) < 2:
-            print("Please enter a value for lastname. No space or intials")
-            get_last_name()
-
-        return last_name
-
-    def get_username():
-        user_name = str(input("Username: ")).strip()
-        if len(user_name) < 5:
-            print("username must be upto 5 characters")
-            get_username()
-
-        return user_name
-
-    def verify_username(user_name):
-        global firstname
-        global lastname
-        global username
-        while (user_name.lower() == firstname.lower()) or (user_name.lower() == lastname.lower()):
-            global username
-            print("firstname or lastname cannot be used as the username")
-            user_name = get_username()
-
-        username = user_name  # assign the accepted username to our global
-
-    firstname = get_first_name()
-    lastname = get_last_name()
-    username = get_username()
-    verify_username(username)  # verify if the user used first or last name as the user name
-    flag = "Unlocked"  # a new account is not supposed to be locked
-
-    # check if username already exist, if it does, request that the user enters an new username
-
-    accounts[username] = (flag, lastname, firstname)  # add the newly created account to account dictionary
-
-    # after creating a new account, add it to the account file
-    with open(path, 'a+') as file:
-        file.write(username + ":" + flag + ":" + firstname + ":" + lastname + "\n")
-
-    return firstname, lastname, flag
+    """
+    The create_new_account function takes the last 4 of the social security and uses it to create an
+    account number for the user
+    :return:
+    """
 
 
-def login():
-    user_name = input("username: ")
-    password = input("password: ")
+
+def create_social_security_number():
+    social_security_hash_database = get_social_security_number_database()
+    print(social_security_hash_database)
 
 
-def read_account_file():
-    if os.stat(path).st_size != 0:
-        global account_file_do_exist
-        account_file_do_exist = True
-        with open(path, 'r') as account_file:
-            for line in account_file:
-                print(line)
-                colon = ":"
-                try:
-                    first_index = line.index(colon)  # first index for colon
-                    username = line[:first_index]
-
-                    second_index = line[first_index + 1:].index(colon)  # second index for colon
-                    second_index += first_index + 1
-                    flag = line[first_index + 1: second_index]
-
-                    third_index = line[second_index + 1:].index(colon)  # third index for colon
-                    third_index += second_index + 1
-                    first_name = line[second_index + 1:third_index]
-
-                    last_name = line[third_index + 1:]
-
-                    accounts[username] = (flag, first_name, last_name)  # add the names to account dictionary
-
-                    print(accounts.items())
-
-                except ValueError:
-                    account_file_do_exist = False
-                    print("Something went wrong with the account file. Verify if account file is totally empty")
 
 
-    else:
-        print("The account file is empty")
-        menu()
+def get_social_security_number_database():
+    """
+    Obtains the existing social security numbers from database, verifies that the new
+    social security number to be assigned to the user doesn't exist
+    :return:
+    """
+    path = "social_security_number_hash"
+    social_security_hash_database = []
+    try:
+        with open(path, 'r') as file:
+            for ssn in file:
+                social_security_hash_database.append(ssn.rstrip())
+
+        return social_security_hash_database
+    except FileNotFoundError:
+        print('We ran into error while fetching the social security database file')
 
 
-def menu():
-    if not account_file_do_exist:
-        print("WELCOME to the Library of the Universe")
-        print("--------------------------------------")
-        create_new_account()
-    else:
-        try:
-            user_input = int(input("WELCOME to the Library of the Universe\n"
-                                   "--------------------------------------\n"
-                                   "1: Login \n"
-                                   "2: Create a new account "))
 
-            if user_input == 1:
-                login()
+def create_default_social_security_number_database():
+    path = 'social_security_number_hash'
+    random_numbers = set([random.randint(100000000, 999999999) for _ in range(0, 20)])
 
-            elif user_input == 2:
-                create_new_account()
+    try:
+        with open(path, 'w') as file:
+            for number in random_numbers:
+                # numbers was created, but we are going to store the hash in order to save the social security incase
+                #  of data breach
+                hash_object = hashlib.sha512(str(number).encode())
+                file.write(str(hash_object.hexdigest()) + "\n")
 
-            else:
-                print("Enter 1 for Login or 2 to create a new account")
-                menu()
-
-        except ValueError:
-            print("Please only integers are allowed!!!")
-            menu()
+            return True
+    except FileNotFoundError:
+        print('We ran into error trying to write default social security numbers to database')
 
 
-def main():
-    read_account_file()
-    menu()
+create_default_social_security_number_database()
+get_social_security_number_database()
 
 
-if __name__ == "__main__":
-    main()
+
