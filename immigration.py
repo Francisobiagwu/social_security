4376008895b4bf59e02b51a3a301c6fe84f89631
Francis <francisobiagwu@gmail.com>
2018-03-23 21:09:53 -0400

    update

--- immigration.py
+++ immigration.py
@@ -4,11 +4,11 @@ __date__ = 3 / 21 / 2018
 
 import Person
 
-new_baby = Person.Person('Francis', 'Obiagwu')
-another_baby = Person.Person('Bob', 'Smith')
+new_baby = Person.Person('Tony', 'Jackson')
+immigrant = Person.Person('Bob', 'Smith')
 
 # prints the firstname, lastname and the last four of ssn
 print(new_baby.first_name, new_baby.last_name, str(new_baby.social_security_number)[5:9])
-print(another_baby.first_name, another_baby.last_name, str(another_baby.social_security_number)[5:9])
+print(immigrant.first_name, immigrant.last_name, str(immigrant.social_security_number)[5:9])
 
 print(Person.get_world_census())
