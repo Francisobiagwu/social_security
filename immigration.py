9dcb3e94442232081db497806032b7745f7ba35f
Francisobiagwu <francisobiagwu@gmail.com>
2018-04-06 13:14:46 -0400

    Update immigration.py

--- immigration.py
+++ immigration.py
@@ -4,12 +4,20 @@ __date__ = 3 / 21 / 2018
 
 from Database import Database, Person
 
-our_database = Database()
-new_baby = Person('Tony', 'Jackson', our_database)
-immigrant = Person('Bob', 'Smith', our_database)
 
-# prints the firstname, lastname and the last four of ssn
-print(new_baby.first_name, new_baby.last_name, str(new_baby.social_security_number)[5:9])
-print(immigrant.first_name, immigrant.last_name, str(immigrant.social_security_number)[5:9])
-
-print(our_database.world_census)
+def main():
+  """
+  This is our main program
+  """
+  our_database = Database()
+  new_baby = Person('Tony', 'Jackson', our_database)
+  immigrant = Person('Bob', 'Smith', our_database)
+  # prints the firstname, lastname and the last four of ssn
+  print(new_baby.first_name, new_baby.last_name, str(new_baby.social_security_number)[5:9])
+  print(immigrant.first_name, immigrant.last_name, str(immigrant.social_security_number)[5:9])
+  print(our_database.world_census)
+  
+  
+  
+if __nam__ =="__main__":
+  main()
